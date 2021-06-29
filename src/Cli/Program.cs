f736d555af2b4a032382d8b75659baf04035976e
David Palumbo <david.palumbo@hotmail.com>
2021-06-29 01:10:42 -0400

    Add CLI project.

---
+++ src/Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IterationZero.Workspace.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
