3ade01aa65841fb8ea0aa501784a838c3fd9081c
David Palumbo <david.palumbo@hotmail.com>
2022-04-26 23:36:12 -0400

    Add assembly version command.

--- src/Commands/Project/AssemblyVersion/AssemblyVersionCliCommand.cs
+++ src/Commands/Project/AssemblyVersion/AssemblyVersionCliCommand.cs
@@ -1,4 +1,6 @@
-﻿using McMaster.Extensions.CommandLineUtils;
+﻿using System;
+
+using McMaster.Extensions.CommandLineUtils;
 
 namespace AggregateGroot.CliTools.Commands.Project.AssemblyVersion
 {
@@ -8,5 +10,25 @@ namespace AggregateGroot.CliTools.Commands.Project.AssemblyVersion
     [Command("assembly-version", Description = "Get the version of an assembly in a project.")]
     public class AssemblyVersionCliCommand : CliCommand
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="AssemblyVersionCliCommand"/> class.
+        /// </summary>
+        public AssemblyVersionCliCommand(IConsole console, IPrompt prompt)
+        {
+            _console = console 
+                ?? throw new ArgumentNullException(nameof(console));
+            _prompt = prompt
+                ?? throw new ArgumentNullException(nameof(prompt));
+        }
+
+        /// <inheritdoc />
+        public override int OnExecute(CommandLineApplication app)
+        {
+            app.ShowHelp();
+            return 1;
+        }
+
+        private readonly IConsole _console;
+        private readonly IPrompt _prompt;
     }
 }
---
+++ src/Tests/Commands.Tests.Unit/Project/AssemblyVersionTests/ConstructorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xunit;
+
+namespace AggregateGroot.Workspace.Cli.Commands.Tests.Unit.Project.AssemblyVersionTests
+{
+    /// <summary>
+    /// Unit tests for the constructors of the <see cref="AssemblyVersion" /> class.
+    /// </summary>
+    public class ConstructorTest
+    {
+       
+    }
+}
